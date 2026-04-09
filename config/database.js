5e819ca130b57f3758347340372d876472ef1541
Ayush Khatri <ayushkhatri@Ayushs-MacBook-Air.local>
2026-04-09 21:20:23 +0545

    db update

--- config/database.js
+++ config/database.js
@@ -6,10 +6,13 @@ dotenv.config();
 // mongoose url from .env file
 const Url = process.env.MONGO_URL
 
-const connectionDB =() =>{
-    mongoose.connect(Url)
-    .then(() => console.log("Databse Connected Successfully..."))
-    .catch((err) => console.log(err ||  "Something went Wrong"))
-}
+const connectionDB = async () => {
+    try {
+        await mongoose.connect(Url);
+        console.log("Database Connected Successfully...");
+    } catch (err) {
+        console.log(err || "Something went Wrong");
+    }
+};
 
 export default connectionDB
